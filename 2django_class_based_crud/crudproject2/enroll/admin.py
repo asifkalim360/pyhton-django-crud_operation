785d33536b0a6d60198c1bb398c1f41fec3e784a
asifkalim360 <110036251+asifkalim360@users.noreply.github.com>
2022-08-03 12:05:12 +0530

    Delete 2django_class_based_crud directory

--- 2django_class_based_crud/crudproject2/enroll/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User            #model se user ko import karna hoga
-# Register your models here.
-@admin.register(User)               #register karna hoga apne User ko.
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id','name','email','password')
